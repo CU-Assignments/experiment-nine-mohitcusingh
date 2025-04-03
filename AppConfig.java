280c2210f463a7b09097ae4227e1b0bad5f359f6
mohitcusingh <165380005+mohitcusingh@users.noreply.github.com>
2025-04-03 23:43:47 +0530

    Create AppConfig.java

---
+++ AppConfig.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Java Programming", 6);
+    }
+
+    @Bean
+    public Student student() {
+        return new Student("John Doe", course());
+    }
+}
